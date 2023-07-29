a550c2a94e90d69c34bf761f5a5fd2a9f489f100
Habeebllahi Afolabi <afolabihabeeb1@gmail.com>
2023-07-29 13:07:47 +0100

    Add project files.

---
+++ DSA/Program.cs
@@ -0,0 +1,12 @@
+﻿using DSA.Solutions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var res = new TwoSumSolution();
+        int[] nums = new[] { 3, 2, 4 };
+        var r = res.TwoSum(nums, 6);
+        Console.WriteLine("{0}, {1}", r[0], r[1]);
+    }
+}
---
+++ DSA/Solutions/TwoSumSolution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Solutions
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int o = 0; o < nums.Length; o++)
+            {
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (o != i && (nums[o] + nums[i]) == target)
+                    {
+                        return new[] { o, i };
+                    }
+                }
+            }
+            return new int[0];
+        }
+
+        public int[] TwoSumSortApproach(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+
+                map[nums[i]] = i;
+            }
+            return new int[0];
+        }
+    }
+}
